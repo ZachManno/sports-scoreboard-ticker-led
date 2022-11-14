ace67c7ec866db612609c7557f8ed9b37379e8be
Zach Manno <zacharymanno@gmail.com>
2022-11-13 21:22:52 -0500

    add live updates

--- espn-scores/led_ticker.py
+++ espn-scores/led_ticker.py
@@ -29,55 +29,46 @@ class GraphicsRunner(SampleBase):
         blue = graphics.Color(0, 255, 213)
 
         yellow = graphics.Color(255, 255, 0)
-        home_score = ''
-        away_score = ''
-        final = ''
 
         while True:
-            scoreboard_index = 0
-            current_games = call_espn_api_and_load_scoreboard()
-            length_of_games_today = len(current_games)
-            scoreboard = current_games[scoreboard_index]
-            home_score = scoreboard.home_team.score
-            home_name = scoreboard.home_team.city_abbr
-            if len(home_name) == 2:
-                home_name += ' '
-            away_name = scoreboard.away_team.city_abbr
-            if len(away_name) == 2:
-                away_name += ' '
-            away_score = scoreboard.away_team.score
-            #home_score = "PHI 32"
-            #away_score = "MIN 22"
-            #final = "FINAL"
-            if rotation % 2 == 0:
-                graphics.DrawText(offscreen_canvas, font, 2, 9, blue, home_name + ' ' + home_score)
-                graphics.DrawText(offscreen_canvas, font, 2, 20, blue, away_name + ' ' + away_score)
-                if scoreboard.gameclock.time_state == TimeState.FINAL:
-                    graphics.DrawText(offscreen_canvas, font, 2, 30, blue, 'FINAL')
-                elif scoreboard.gameclock.time_state == TimeState.LIVE:
-                    graphics.DrawText(offscreen_canvas, font, 2, 30, blue, quarter_map[scoreboard.gameclock.live_period] + ' ' + scoreboard.gameclock.live_clock)
-            else:
-                #home_score = "HHI 32"
-                #away_score = "NNN 22"
+            for scoreboard in call_espn_api_and_load_scoreboard():
+                home_score = scoreboard.home_team.score
+                home_name = scoreboard.home_team.city_abbr
+                if len(home_name) == 2:
+                    home_name += ' '
+                away_name = scoreboard.away_team.city_abbr
+                if len(away_name) == 2:
+                    away_name += ' '
+                away_score = scoreboard.away_team.score
+                #home_score = "PHI 32"
+                #away_score = "MIN 22"
                 #final = "FINAL"
-                graphics.DrawText(offscreen_canvas, font, 2, 9, green, home_name + ' ' + home_score)
-                graphics.DrawText(offscreen_canvas, font, 2, 20, green, away_name + ' ' + away_score)
-                if scoreboard.gameclock.time_state == TimeState.FINAL:
-                    graphics.DrawText(offscreen_canvas, font, 2, 30, green, 'FINAL')
-                elif scoreboard.gameclock.time_state == TimeState.LIVE:
-                    graphics.DrawText(offscreen_canvas, font, 2, 30, green, quarter_map[scoreboard.gameclock.live_period] + ' ' + scoreboard.gameclock.live_clock)
+                if rotation % 2 == 0:
+                    graphics.DrawText(offscreen_canvas, font, 2, 9, blue, home_name + ' ' + home_score)
+                    graphics.DrawText(offscreen_canvas, font, 2, 20, blue, away_name + ' ' + away_score)
+                    if scoreboard.gameclock.time_state == TimeState.FINAL:
+                        graphics.DrawText(offscreen_canvas, font, 2, 30, blue, 'FINAL')
+                    elif scoreboard.gameclock.time_state == TimeState.LIVE:
+                        graphics.DrawText(offscreen_canvas, font, 2, 30, blue, quarter_map[scoreboard.gameclock.live_period] + ' ' + scoreboard.gameclock.live_clock)
+                else:
+                    #home_score = "HHI 32"
+                    #away_score = "NNN 22"
+                    #final = "FINAL"
+                    graphics.DrawText(offscreen_canvas, font, 2, 9, green, home_name + ' ' + home_score)
+                    graphics.DrawText(offscreen_canvas, font, 2, 20, green, away_name + ' ' + away_score)
+                    if scoreboard.gameclock.time_state == TimeState.FINAL:
+                        graphics.DrawText(offscreen_canvas, font, 2, 30, green, 'FINAL')
+                    elif scoreboard.gameclock.time_state == TimeState.LIVE:
+                        graphics.DrawText(offscreen_canvas, font, 2, 30, green, quarter_map[scoreboard.gameclock.live_period] + ' ' + scoreboard.gameclock.live_clock)
 
-            # graphics.DrawText(offscreen_canvas, weather_font, 40, 6, yellow, self.philly_weather.condition.text)
-            # graphics.DrawText(offscreen_canvas, weather_font, 55, 13, yellow, str(self.philly_weather.condition.temperature))
-            scoreboard_index = scoreboard_index + 1
-            if scoreboard_index == length_of_games_today:
-                scoreboard_index = 0
-            rotation = rotation + 1
-            if rotation == 90000:
-                rotation = 0
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-            offscreen_canvas = self.matrix.CreateFrameCanvas()
-            time.sleep(5)
+                # graphics.DrawText(offscreen_canvas, weather_font, 40, 6, yellow, self.philly_weather.condition.text)
+                # graphics.DrawText(offscreen_canvas, weather_font, 55, 13, yellow, str(self.philly_weather.condition.temperature))
+                rotation = rotation + 1
+                if rotation == 90000:
+                    rotation = 0
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                offscreen_canvas = self.matrix.CreateFrameCanvas()
+                time.sleep(5)
 
 
 # Main function
