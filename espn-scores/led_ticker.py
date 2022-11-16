e650673dc849885a123026a73376a713f0419593
Zach Manno <zacharymanno@gmail.com>
2022-11-16 18:48:04 -0500

    font

--- espn-scores/led_ticker.py
+++ espn-scores/led_ticker.py
@@ -32,12 +32,12 @@ class GraphicsRunner(SampleBase):
         # Home
         graphics.DrawText(offscreen_canvas, self.medium_font, 2, 9, color,
                           self.format_team_abbr(scoreboard.home_team.city_abbr) + ' ' + scoreboard.home_team.score)
-        graphics.DrawText(offscreen_canvas, self.smallest_font, 36, 9, self.white, scoreboard.home_team.record)
+        graphics.DrawText(offscreen_canvas, self.smallest_font, 31, 9, self.white, scoreboard.home_team.record)
         graphics.DrawText(offscreen_canvas, self.medium_font, 70, 9, self.yellow, '1st and 10')
         # Away
         graphics.DrawText(offscreen_canvas, self.medium_font, 2, 20, color,
                           self.format_team_abbr(scoreboard.away_team.city_abbr) + ' ' + scoreboard.away_team.score)
-        graphics.DrawText(offscreen_canvas, self.smallest_font, 36, 20, self.white, scoreboard.away_team.record)
+        graphics.DrawText(offscreen_canvas, self.smallest_font, 31, 20, self.white, scoreboard.away_team.record)
         if scoreboard.gameclock.time_state:
             if scoreboard.gameclock.time_state == TimeState.FINAL:
                 graphics.DrawText(offscreen_canvas, self.medium_font, 2, 30, color, 'FINAL')
