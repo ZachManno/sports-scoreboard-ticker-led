e208f2298924ae3e3f0cde24388c87c360d09c8a
Zach Manno <zacharymanno@gmail.com>
2022-11-16 18:06:14 -0500

    test draw

--- espn-scores/led_ticker.py
+++ espn-scores/led_ticker.py
@@ -31,6 +31,7 @@ class GraphicsRunner(SampleBase):
         graphics.DrawText(offscreen_canvas, self.font, 2, 9, color,
                           self.format_team_abbr(scoreboard.home_team.city_abbr) + ' ' + scoreboard.home_team.score)
         graphics.DrawText(offscreen_canvas, self.font, 36, 9, self.white, scoreboard.home_team.record)
+        graphics.DrawText(offscreen_canvas, self.font, 80, 9, self.white, scoreboard.home_team.record)
         # Away
         graphics.DrawText(offscreen_canvas, self.font, 2, 20, color,
                           self.format_team_abbr(scoreboard.away_team.city_abbr) + ' ' + scoreboard.away_team.score)
