005c0c629ec057db72dd409981f180197eaae6ce
Zach Manno <zacharymanno@gmail.com>
2022-11-16 20:58:23 -0500

    adjust

--- espn-scores/led_ticker.py
+++ espn-scores/led_ticker.py
@@ -32,19 +32,19 @@ class GraphicsRunner(SampleBase):
     def draw_image(self, offscreen_canvas):
         image = Image.open('images/nfl-2.png').convert('RGB')
         image.thumbnail((16, 16), Image.ANTIALIAS)
-        offscreen_canvas.SetImage(image, 46)
+        offscreen_canvas.SetImage(image, 48)
         print()
 
     def write_scoreboard(self, offscreen_canvas, color, scoreboard):
         # Home
         graphics.DrawText(offscreen_canvas, self.medium_font, 2, 9, color,
                           self.format_team_abbr(scoreboard.home_team.city_abbr) + ' ' + '28')
-        graphics.DrawText(offscreen_canvas, self.smallest_font, 33, 9, self.white, scoreboard.home_team.record)
+        graphics.DrawText(offscreen_canvas, self.smallest_font, 34, 9, self.white, scoreboard.home_team.record)
         # graphics.DrawText(offscreen_canvas, self.medium_font, 70, 9, self.yellow, '1st and 10')
         # Away
         graphics.DrawText(offscreen_canvas, self.medium_font, 2, 20, color,
                           self.format_team_abbr(scoreboard.away_team.city_abbr) + ' ' + '17')
-        graphics.DrawText(offscreen_canvas, self.smallest_font, 33, 20, self.white, scoreboard.away_team.record)
+        graphics.DrawText(offscreen_canvas, self.smallest_font, 34, 20, self.white, scoreboard.away_team.record)
         if scoreboard.gameclock.time_state:
             if scoreboard.gameclock.time_state == TimeState.FINAL:
                 graphics.DrawText(offscreen_canvas, self.medium_font, 2, 30, color, 'FINAL')
