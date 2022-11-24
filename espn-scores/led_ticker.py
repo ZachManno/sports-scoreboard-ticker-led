861481554240b45423c78844c9a9a4c969a9bcdf
Zach Manno <zacharymanno@gmail.com>
2022-11-24 11:25:53 -0500

    football field

--- espn-scores/led_ticker.py
+++ espn-scores/led_ticker.py
@@ -90,7 +90,7 @@ class GraphicsRunner(SampleBase):
 
         # Test drawing field goal posts and green
         scoreboard.gameclock.game_situation = GameSituation(down_and_distance="3rd and 7", home_team_has_ball=False,
-                                                            away_team_has_ball=True, ball_on_yardline=1,
+                                                            away_team_has_ball=True, ball_on_yardline=25,
                                                             ball_on_team='BUF')
         self.draw_football_field(offscreen_canvas, scoreboard)
 
